975cb16857949b460a17ad8022d80e5b0ed0c9bd
sabts <sabrinataraschi@gmail.com>
2025-05-20 00:26:06 +0200

    Update PlanetCard.jsx

--- src/components/planet-card/PlanetCard.jsx
+++ src/components/planet-card/PlanetCard.jsx
@@ -1,40 +1,35 @@
-const PlanetCard = () => {
-    return (
-      <>
-        <img src="/assets/planet-mercury.svg" alt="Mercury" />
-  
-        <h2>MERCURY</h2>
-        <p>
-          Mercury is the smallest planet in the Solar System and the closest to
-          the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest
-          of all the Sun's planets. Mercury is one of four terrestrial planets in
-          the Solar System, and is a rocky body like Earth.
-        </p>
-  
-        <a href="https://es.wikipedia.org/wiki/Mercurio_(planeta)" target="_blank" rel="noopener noreferrer">
-          Source: Wikipedia
-        </a>
-  
-        <section>
-          <div>
-            <span>ROTATION TIME</span>
-            <span>58.6 days</span>
-          </div>
-          <div>
-            <span>REVOLUTION TIME</span>
-            <span>87.97 days</span>
-          </div>
-          <div>
-            <span>RADIUS</span>
-            <span>2,439.7 km</span>
-          </div>
-          <div>
-            <span>AVERAGE TEMP.</span>
-            <span>430°C</span>
-          </div>
-        </section>
-      </>
-    );
-  };
-  
-  export default PlanetCard;
+const PlanetCard = ({ name, image, description, rotationTime, revolutionTime, radius, averageTemp,wikiUrl }) => {
+  return (
+    <div className="planet-card">
+      <img src={image} alt={name} />
+
+      <h2>{name.toUpperCase()}</h2>
+      <p>{description}</p>
+
+      <a href={wikiUrl}>
+        Source: Wikipedia
+      </a>
+
+      <section>
+        <div>
+          <span>ROTATION TIME</span>
+          <span>{rotationTime}</span>
+        </div>
+        <div>
+          <span>REVOLUTION TIME</span>
+          <span>{revolutionTime}</span>
+        </div>
+        <div>
+          <span>RADIUS</span>
+          <span>{radius}</span>
+        </div>
+        <div>
+          <span>AVERAGE TEMP.</span>
+          <span>{averageTemp}</span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PlanetCard;
