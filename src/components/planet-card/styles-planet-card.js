a5411eda76542c636024737594c296541db86d76
sabts <sabrinataraschi@gmail.com>
2025-05-21 11:22:07 +0200

    .
    cambiar:
    -Ajustar los objetos del planeta y tabs
    -ajustar el div de StyledPhotoMainContainer

--- src/components/planet-card/styles-planet-card.js
+++ src/components/planet-card/styles-planet-card.js
@@ -10,54 +10,60 @@ const StyledPlanetContainer = styled.div`
   padding-inline: 1.5rem;
 `;
 
-const StyledSectionContainer = styled.div `
-display: flex;
-justify-content: space-between;
-align-items: center;
-border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-font-size: 0.5625rem;
-font-weight: 700;
-letter-spacing: 0.1206rem;
-color: #fff;
-height: 55px;
-padding: 1.5rem;
-`
+const StyledSectionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  font-size: 0.5625rem;
+  font-weight: 700;
+  letter-spacing: 0.1206rem;
+  color: #fff;
+  height: 55px;
+  padding: 1.5rem;
+`;
 const StyledSectionButton = styled.span`
   display: flex;
   align-items: center;
   height: 55px;
   padding: 10px 15px;
   height: strech;
-  color: ${({ $isActive }) => ($isActive ? '#fff' : 'rgba(255, 255, 255, 0.2)')};
+  color: ${({ $isActive }) =>
+    $isActive ? "#fff" : "rgba(255, 255, 255, 0.2)"};
   border-bottom: 4px solid
-    ${({ $isActive, $planetColor }) => ($isActive ? $planetColor : 'transparent')};
+    ${({ $isActive, $planetColor }) =>
+      $isActive ? $planetColor : "transparent"};
   transition: all 0.2s ease-in-out;
 `;
 
-//cada planeta tiene un tamaño colocarlo en el objeto $planetSize y un before para el surface $planetLocation
-const StyledPlanetPhoto = styled.div`
+const StyledPhotoMainContainer = styled.div`
   width: ${({ $planetSize }) => $planetSize};
   height: ${({ $planetSize }) => $planetSize};
   margin: 50px;
   position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 95%;
+    left: 50%;
+    width: 70%;
+    height: 70%;
+    background-image: url(${({ $planetLocation }) => $planetLocation});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const StyledPlanetPhoto = styled.img`
+  width: ${({ $planetSize }) => $planetSize};
+  height: ${({ $planetSize }) => $planetSize};
   background-image: url(${({ src }) => src});
   background-size: cover;
   background-position: center;
   display: block;
-
-   &::after {
-  content: "";
-  position: absolute;
-  top: 95%;
-  left: 50%;
-  width: 70%;
-  height: 70%;
-  background-image: url(${({ $planetLocation }) => $planetLocation});
-  background-size: contain;
-  background-repeat: no-repeat; 
-  background-position: center;
-  transform: translate(-50%, -50%);
-  }
 `;
 
 const StyledPlanetInfoContainer = styled.div`
@@ -66,11 +72,11 @@ const StyledPlanetInfoContainer = styled.div`
 `;
 
 const StyledPlanetNameTitle = styled.h2`
-color: #fff;
-font-size: 2.5rem;
-font-weight: 400;
-text-transform: uppercase;
-`
+  color: #fff;
+  font-size: 2.5rem;
+  font-weight: 400;
+  text-transform: uppercase;
+`;
 
 const StyledInfoText = styled.p`
   font-size: 0.6875rem;
@@ -82,15 +88,15 @@ const StyledSourceContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 28px
-`
+  margin-bottom: 28px;
+`;
 
 const StyledSource = styled.span`
   color: rgba(255, 255, 255, 0.2);
   font-size: 0.75rem;
   font-weight: 400;
   line-height: 1.5625rem;
-`
+`;
 
 const StyledLinkSource = styled(Link)`
   color: rgba(255, 255, 255, 0.2);
@@ -98,7 +104,7 @@ const StyledLinkSource = styled(Link)`
   font-weight: 700;
   line-height: 1.5625rem;
   text-decoration-line: underline;
-`
+`;
 
 const StyledExtraInfobox = styled.div`
   display: flex;
@@ -118,19 +124,20 @@ const StyledExtraInfobox = styled.div`
 `;
 
 const StyledExtraInfoCaption = styled.span`
-color: rgba(255, 255, 255, 0.2);
-font-size: 0.5rem;
-font-style: normal;
-font-weight: 700;
-line-height: 1rem;
-letter-spacing: 0.0454rem;
-text-transform: uppercase;
-`
+  color: rgba(255, 255, 255, 0.2);
+  font-size: 0.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1rem;
+  letter-spacing: 0.0454rem;
+  text-transform: uppercase;
+`;
 
 export {
   StyledPlanetContainer,
   StyledSectionContainer,
   StyledSectionButton,
+  StyledPhotoMainContainer,
   StyledPlanetPhoto,
   StyledPlanetInfoContainer,
   StyledPlanetNameTitle,
